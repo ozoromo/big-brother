8bd85b9d2d43203e2a01d51ff91ea45dd7bfbc4c
ND-2002 <105594962+ND-2002@users.noreply.github.com>
2023-06-26 18:24:44 +0200

    Revert "signup"
    This reverts commit a16c764e737682e8c7494f7b373aeb0028ae9941.

--- src/bigbrother/app/__init__.py
+++ src/bigbrother/app/__init__.py
@@ -300,6 +300,8 @@ def create():
         user = {
                     'username': form.name.data,
                     'pic1' : request.files['pic1'],
+                    'pic2' : request.files['pic2'],
+                    'pic3' : request.files['pic3']
                 }
 
         user_uuid = None
@@ -313,7 +315,7 @@ def create():
             #pic_2 = form.pictureleft.data
             #pic_3 = form.pictureright.data
 
-            pictures = [user['pic1']]
+            pictures = [user['pic1'],user['pic2'],user['pic3']]
 
             # TODO pls change last parameter
             user_uuid = ws.DB.register_user(user['username'], None)
