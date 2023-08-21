fe051eba3c0d0863cfc43c77422a83e610937ef0
mike0609king <mike-huang@web.de>
2023-08-21 16:27:00 +0200

    Small changes

--- src/eduVid/question_answering/example_usage/qa_usage.py
+++ src/eduVid/question_answering/example_usage/qa_usage.py
@@ -1,29 +1,25 @@
-from moviepy.video.io.VideoFileClip import VideoFileClip
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering
-import torch
-from faster_whisper import WhisperModel
+import sys
+import os
 
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from qa_algo_core import HelperFN, SpeechRecog, QAAlgo
 
-
-model_name = "timpal0l/mdeberta-v3-base-squad2"  # set up hugging face model name
+# set up hugging face model name
+model_name = "timpal0l/mdeberta-v3-base-squad2"
 
 input_mp4_file = "audio.mp4"
+wav_file_name = "audio.wav"
 
-wav_file_name = "audio.wav"  # location and name of converted audio file
 audio_wav = HelperFN.extract_audio_from_mp4(input_mp4_file, wav_file_name)
 
-recog = SpeechRecog("audio.wav")  # set the audio file which will be transcribed
-
+recog = SpeechRecog(wav_file_name)  # set the audio file which will be transcribed
 context, tags = recog.transcribe()
-
-question = "Was sind die grundlegenden Mechanismen, die in Prolog angewendet werden?"  # ask your question
-
+question = "Was sind die grundlegenden Mechanismen, die in Prolog angewendet werden?"
 
 qa_result = QAAlgo(model_name)
 answer = qa_result.answer_question(context, question)
-
 matching_segments = HelperFN.find_matching_segments(tags, answer)
+# Returns a list with time intervals at which the answer might be
 merged_segments = HelperFN.merge_overlapping_segments(matching_segments)
 
 print("\nQuestion:", question)
