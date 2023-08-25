c91386fafb6c2cb6ab465e24e0f32fdc877bbd22
mike0609king <mike-huang@web.de>
2023-08-25 20:49:29 +0200

    Moved images and examples

--- docs/examples/face_recog/face_recognition_lib/example_usage_FaceReco_class.py
+++ docs/examples/face_recog/face_recognition_lib/example_usage_FaceReco_class.py
@@ -13,8 +13,8 @@ import face_recognition
 import FaceReco_class
 
 f = FaceReco_class.FaceReco()
-testing_image = face_recognition.load_image_file("../../../../res/testImages/merkel.jpg")
-training_image = face_recognition.load_image_file("../../../../res/testImages/merkel2.jpg") # image from DB
+testing_image = face_recognition.load_image_file("../../../../res/test_images/merkel.jpg")
+training_image = face_recognition.load_image_file("../../../../res/test_images/merkel2.jpg") # image from DB
 
 image_encoding = face_recognition.face_encodings(training_image)
 print(f.photo_to_photo(image_encoding[0], testing_image))
