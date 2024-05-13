52922ebc5cf0869f186a1a97f07a22d6fa32c3b1
ozoromo <dev@ozoromo.com>
2024-05-13 15:52:29 +0200

    Started work on gesture model replacement

--- src/gesture_recognition/gesture_recognizer.py
+++ src/gesture_recognition/gesture_recognizer.py
@@ -1,36 +1,68 @@
 import os
 
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
 import cv2
 import numpy as np
 import mediapipe as mp
-import tensorflow as tf
-from tensorflow.keras.models import load_model
+import torch
+from albumentations.pytorch import ToTensorV2
+
+COLOR = (0, 255, 0)
+FONT = cv2.FONT_HERSHEY_SIMPLEX
 
 
 class GestureRecognizer:
     def __init__(self):
         # initialize mediapipe
         self.mp_hands = mp.solutions.hands
-        self.hands = self.mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+        self.hands = self.mp_hands.Hands(
+            model_complexity=0, static_image_mode=False, max_num_hands=2, min_detection_confidence=0.8
+        )
         self.mp_draw = mp.solutions.drawing_utils
+        self.conf = OmegaConf.load()
 
         # initialize tensorflow
         # Load the gesture recognizer model
 
-        path = os.path.dirname(os.path.abspath(__file__))
-        model_dir = os.path.abspath(os.path.join(path, 'mp_hand_gesture'))
-        self.model = load_model(model_dir)
+    def preprocess(img: np.ndarray, transform) -> Tuple[Tensor, Tuple[int, int], Tuple[int, int]]:
+        """
+        Preprocess the image before feeding it to the model.
+        Parameters
+        ----------
+        img: np.ndarray
+            input image
+        transform :
+            albumentation transforms
+        """
+        height, width = img.shape[0], img.shape[1]
+        transformed_image = transform(image=img)
+        processed_image = transformed_image["image"] / 255.0
+        return processed_image, (width, height)
 
-        gesture_dir = os.path.abspath(os.path.join(path, 'gesture.names'))
 
-        # Load class names
-        with open(gesture_dir, 'r') as f:
-            self.class_names = f.read().split('\n')
+    def get_transform_for_inf(transform_config: DictConfig):
+        transforms_list = [getattr(A, key)(**params) for key, params in transform_config.items()]
+        transforms_list.append(ToTensorV2())
+        return A.Compose(transforms_list)
+
 
     def recognize(self, frame):
         """
-        Receive a frame, then return the frame with the recognized hand gesture highlighted as well as the name of the gesture.
+        Receive a frame, preprocess it and return 
         """
+
+        processed_image, size = GestureRecognizer.preprocess(frame, transform)
+                with torch.no_grad():
+                    output = detector([processed_image])[0]
+                boxes = output["boxes"][:num_hands]
+                scores = output["scores"][:num_hands]
+                labels = output["labels"][:num_hands]
+
+
+        return frame, class_name
+
         x, y, c = frame.shape
 
         # Flip the frame vertically
@@ -61,4 +93,5 @@ class GestureRecognizer:
                 prediction = self.model.predict([landmarks])
                 class_id = np.argmax(prediction)
                 class_name = self.class_names[class_id]
-        return frame, class_name
+
+
